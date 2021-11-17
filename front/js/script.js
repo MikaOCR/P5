10bbcfe45614fba1cad578f61020abc814215507
Michael <michael.caron.dev@gmail.com>
2021-11-17 16:56:09 +0100

    Ajout des cartes produits en JS

---
+++ front/js/script.js
@@ -0,0 +1,32 @@
+let productData = [];
+
+const fetchProduct = async () => {
+    await fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((promise) => {
+        productData = promise
+        console.log(productData);
+    });
+};
+
+const productCard = async () => {        /* Carte des produits Index.html */
+    await fetchProduct();
+
+    document.getElementById("items").innerHTML = productData.map((items) => `
+    <a href="./product.html?id=42">
+
+    <article id="items${items._id}" class="items">
+        <img class="item__img" src="${items.imageUrl}" alt="${items.altTxt}"/>
+        <h3 class="productName">${items.name.toUpperCase()}</h3>
+        <p class="productDescription">${items.description}</p>
+    </article>
+
+    </a>
+    `,
+      )
+      .join("")  /* Pour enlever les virgules */
+};
+
+
+
+productCard();   /* On appel la fonction */
